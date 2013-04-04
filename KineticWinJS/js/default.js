53ddda0321ee38aadf08c7dce0894cf68f1c1b68
CoryG89 <CoryG89@gmail.com>
2013-04-04 01:07:27 -0500

    Initial Commit

---
+++ KineticWinJS/js/default.js
@@ -0,0 +1,99 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize
+                // your application here.
+
+                /** Create KineticJS Stage */
+                initStage();
+
+            } else {
+                // TODO: This application has been reactivated from suspension.
+                // Restore application state here.
+            }
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. You might use the
+        // WinJS.Application.sessionState object, which is automatically
+        // saved and restored across suspension. If you need to complete an
+        // asynchronous operation before your application is suspended, call
+        // args.setPromise().
+    };
+
+    app.start();
+
+
+    function initStage() {
+        var stage = new Kinetic.Stage({
+            width: 800, height: 600, container: 'container'
+        });
+
+        var bgLayer = new Kinetic.Layer();
+        var mainLayer = new Kinetic.Layer();
+
+        var backgroundRect = new Kinetic.Rect({
+            width: 800, height: 600, fill: 'white'
+        });
+        bgLayer.add(backgroundRect);
+
+        var bouncer = new Kinetic.Rect({
+            x: Math.max(0, Math.floor(Math.random() * 800 - 50)),
+            y: Math.max(0, Math.floor(Math.random() * 600 - 50)),
+            width: 50, height: 50, fill: 'black'
+        });
+
+        bouncer.bounds = {
+            top: 0, left: 0,
+            right: stage.getWidth() - bouncer.getWidth(),
+            bottom: stage.getHeight() - bouncer.getHeight()
+        };
+
+        bouncer.collision = function () {
+            if (this.getX() <= this.bounds.left) return 'left'
+            if (this.getX() >= this.bounds.right) return 'right'
+            if (this.getY() <= this.bounds.top) return 'top'
+            if (this.getY() >= this.bounds.bottom) return 'bottom'
+        };
+        bouncer.direction = 2 * Math.PI * Math.random();
+        bouncer.velocity = {
+            x: 20 * Math.cos(bouncer.direction),
+            y: 20 * Math.sin(bouncer.direction)
+        };
+
+        var bouncerAnim = new Kinetic.Animation(function (frame) {
+            var delta = frame.timeDiff / 1000;
+
+            var collision = bouncer.collision();
+            if (collision === 'left' || collision === 'right')
+                bouncer.velocity.x = -bouncer.velocity.x;
+            if (collision === 'top' || collision == 'bottom')
+                bouncer.velocity.y = -bouncer.velocity.y;
+
+            bouncer.setX(bouncer.getX() + delta * bouncer.velocity.x);
+            bouncer.setY(bouncer.getY() + delta * bouncer.velocity.y);
+
+        }, mainLayer);
+        
+        mainLayer.add(bouncer);
+
+        stage.add(bgLayer);
+        stage.add(mainLayer);
+
+        bouncerAnim.start();
+    }
+
+})();
---
+++ README.md
@@ -0,0 +1,40 @@
+# KineticWinJS Boilerplate
+###### Written by Cory Gross
+
+Meant to be used as a starter boilerplate app for Windows 8 games developed for
+for the Windows Store written in JavaScript / HTML5. Uses the KineticJS Canvas
+API. Currently has no other dependencies other than KineticJS and the WinJS
+JavaScript Library included in the Windows SDK.
+
+## Files Of Interest
+
+ * **default.html**
+   - This is your app's main page. I just have some very basic markup in here
+     currently that creates the header and the container for the KineticJS canvas.
+     You can see that the WinJS dependencies and such are also loaded in here. Any
+     JavaScript your write will need to be linked in here with the script tag. You
+     have access to the [WinJS CSS classes][1] for use in your markup. You may
+     also use [Blend for Visual Studio 2012][2] in order to more easily work with
+     the Windows Grid UI style.
+
+ * **default.js**
+  - This is where WinJS is initialized, your have some events in here that get
+    fired off that we can take note of.
+  - `app.oncheckpoint` is fired off just before your app is suspended so that
+    is where you should save any data, or perhaps pause or game.
+  - `app.onactivated` is called when your app is loaded up and becomes active,
+    it has a parameter named `args` which is examined to determine the previous
+    execution state in order to determine if this is the first activation or if
+    the app is being woken from suspension.
+  - The `app.onactivated` method is where the KineticJS stage is initialized,
+    but only on the first activation.
+
+ * **default.css**
+  - This is where your styling rules go. This is linked to by `default.html`
+    and is defines the width and height of the KineticJS container.
+  - There is also two other CSS files you may utilize. `ui-dark.css` and
+    `ui-light.css` for swapping between the default light and dark themes for
+    Windows 8 apps.
+   
+[1]: http://msdn.microsoft.com/en-us/library/windows/apps/hh770562.aspx
+[2]: http://msdn.microsoft.com/en-us/library/windows/apps/jj129478.aspx
